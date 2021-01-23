8ee18942bd2c27a99d99622eee58b055e704a68d
Justin <student.j.nguyen@horizoncolumbus.org>
2021-01-22 19:42:20 -0500

    friday assignment

--- src/org/horizoncolumbus/hs/Main.java
+++ src/org/horizoncolumbus/hs/Main.java
@@ -5,19 +5,26 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         int[] numbers = new int[5];
-//        int[] numbers = new int[] {1, 2, 3, 4, 5};
 
-//        numbers[0] = 1;
-//        numbers[1] = 2;
-//        numbers[2] = 3;
-//        numbers[3] = 4;
-//        numbers[4] = 5;
-        int sum = 0;
         for(int i = 0; i < 5; i++){
             numbers[i] = i + 1;
+        }
+
+        int sum = 0;
+        for(int i = 0; i < 5; i++){
             sum += numbers[i];
         }
 
-        System.out.println(sum);
+        StringBuilder formula = new StringBuilder();
+        for(int i = 0; i < 5; i++){
+            if (numbers[i] == 5){
+                formula.append(numbers[i]);
+            }
+            else {
+                formula.append(numbers[i]).append(" + ");
+            }
+        }
+
+        System.out.println(formula + " = " + sum);
     }
 }

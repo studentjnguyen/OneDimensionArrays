0ab8dbfd990b46186dd76d4cd0796ace418a927a
Justin <student.j.nguyen@horizoncolumbus.org>
2021-01-25 11:06:20 -0500

    monday assignment

--- src/org/horizoncolumbus/hs/Main.java
+++ src/org/horizoncolumbus/hs/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
         int[] numbers = new int[5];
 
         for(int i = 0; i < 5; i++){
---
+++ src/org/horizoncolumbus/hs/Main3.java
@@ -0,0 +1,25 @@
+package org.horizoncolumbus.hs;
+
+import java.util.Scanner;
+
+public class Main3 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String string = input.nextLine();
+        int arraySize = Integer.parseInt(string);
+        int[] numbers = new int[arraySize];
+
+        for(int i = 0; i < arraySize; i++){
+            numbers[i] = i + 1;
+        }
+
+        int sum = 0;
+        for(int i = 0; i < arraySize; i++){
+            sum += numbers[i];
+        }
+
+        System.out.println(sum);
+        input.close();
+    }
+}
